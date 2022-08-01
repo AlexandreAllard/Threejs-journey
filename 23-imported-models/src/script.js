5954f0331884e98c1c5cb2c8657bdab594e3b8a7
AlexandreAllard <alrdalexandre@gmail.com>
2022-08-01 14:59:58 +0200

    Lesson 23 - DracoLoader addition

--- 23-imported-models/src/script.js
+++ 23-imported-models/src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 /**
  * Base
@@ -20,9 +21,14 @@ const scene = new THREE.Scene()
  * Models
  */
 
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
 const gtlfLoader = new GLTFLoader()
+gtlfLoader.setDRACOLoader(dracoLoader)
+
 gtlfLoader.load(
-    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    '/models/Duck/glTF-Draco/Duck.gltf',
     (gltf) => {
         // const children = [...gltf.scene.children]
         
