a6370e9d89e8a684fd0a9084bc29616166a82810
AlexandreAllard <alrdalexandre@gmail.com>
2022-08-02 18:11:04 +0200

    Lesson 25 - Done

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -62,9 +62,9 @@ gui.add(debugObjects, 'envMapIntensity').min(0).max(10).step(0.001).onChange(upd
  * Models
  */
 gltfLoader.load(
-    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    '/models/hamburger.glb',
     (gltf) => {
-        gltf.scene.scale.set(10,10,10)
+        gltf.scene.scale.set(0.3,0.3,0.3)
         gltf.scene.position.set(0,-4,0)
         gltf.scene.rotation.y = Math.PI * 0.5
 
@@ -86,6 +86,7 @@ directionalLight.position.set(0.25,3,-2.25)
 directionalLight.castShadow = true
 directionalLight.shadow.camera.far = 15
 directionalLight.shadow.mapSize.set(1024,1024)
+directionalLight.shadow.normalBias = 0.02
 scene.add(directionalLight)
 
 // const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
