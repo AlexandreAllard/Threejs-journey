882dd4b9066408340d2e78d98821abf04c444cd2
AlexandreAllard <alrdalexandre@gmail.com>
2022-08-05 21:59:45 +0200

    Lesson 28 - Pattern 23

--- 28-shaders-patterns/src/shaders/test/fragment.glsl
+++ 28-shaders-patterns/src/shaders/test/fragment.glsl
@@ -1,5 +1,10 @@
 varying vec2 vUv;
 
+float random(vec2 st)
+{        
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
 void main()
 {
     //Pattern 3
@@ -81,8 +86,13 @@ void main()
     // //Pattern 21
     // float strength = floor(vUv.x* 10.0) / 10.0;
 
-    //Pattern 22
-    float strength = (floor(vUv.x* 10.0) / 10.0 * floor(vUv.y* 10.0) / 10.0); 
+    // //Pattern 22
+    // float strength = (floor(vUv.x* 10.0) / 10.0 * floor(vUv.y* 10.0) / 10.0); 
+
+    //Pattern 23
+    float strength = random(vUv); 
+
+
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
