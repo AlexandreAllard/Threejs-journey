c812874a9c95e10f37a14bd32ef3204decfc54d5
AlexandreAllard <alrdalexandre@gmail.com>
2022-08-05 23:54:04 +0200

    Lesson 29 - Moving sea

--- 29-raging-sea/src/script.js
+++ 29-raging-sea/src/script.js
@@ -26,8 +26,18 @@ const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 // Material
 const waterMaterial = new THREE.ShaderMaterial({
     vertexShader: waterVertexShader,
-    fragmentShader: waterFragmentShader
+    fragmentShader: waterFragmentShader,
+    uniforms: {
+        uTime: { value: 0 },
+        uBigWavesElevation: { value: 0.2 },
+        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5)}
+    }
 })
+
+//Debug
+gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0.001).name('uBigWavesElevation')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyY')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
@@ -86,6 +96,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Upodate water
+    waterMaterial.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
