9e258a1e38ba12ab9a8e48ad7e69e53cda4c3e1d
AlexandreAllard <alrdalexandre@gmail.com>
2022-08-05 21:50:51 +0200

    Lesson 28 - Pattern 20

--- 28-shaders-patterns/src/shaders/test/fragment.glsl
+++ 28-shaders-patterns/src/shaders/test/fragment.glsl
@@ -69,9 +69,14 @@ void main()
     // float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
 
     //Pattern 19
-    float value = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+    // float value = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
 
-    float strength = step(0.2, value);
+    // float strength = step(0.2, value);
+
+    //Pattern 20
+    float square1 = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    float square2 = 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    float strength = square1 * square2;
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
