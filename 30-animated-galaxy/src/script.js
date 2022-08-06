2c6958efe7726725e15a770533c4f51c23f12525
AlexandreAllard <alrdalexandre@gmail.com>
2022-08-06 13:17:08 +0200

    Lesson 30 - Size attenuation added

--- 30-animated-galaxy/src/script.js
+++ 30-animated-galaxy/src/script.js
@@ -100,7 +100,7 @@ const generateGalaxy = () =>
         fragmentShader: galaxyFragmentShader,
         uniforms:
         {
-            uSize:{ value: 8}
+            uSize:{ value: 8 * renderer.getPixelRatio() }
         }
     })
 
@@ -111,7 +111,6 @@ const generateGalaxy = () =>
     scene.add(points)
 }
 
-generateGalaxy()
 
 gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
 gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
@@ -166,6 +165,12 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Generate galaxy
+ */
+
+ generateGalaxy()
 
 /**
  * Animate
