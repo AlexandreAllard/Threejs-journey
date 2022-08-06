0741340414b095af7d4df92cd89d7dd5aa327f46
AlexandreAllard <alrdalexandre@gmail.com>
2022-08-06 19:46:38 +0200

    Lesson 32 - Drunk pass

--- 32-post-processing/src/script.js
+++ 32-post-processing/src/script.js
@@ -187,6 +187,8 @@ unrealBloomPass.radius = 1
 unrealBloomPass.threshold = 0.6
 effectComposer.addPass(unrealBloomPass)
 
+
+//Unreal bloom pass
 gui.add(unrealBloomPass, 'enabled')
 gui.add(unrealBloomPass, 'strength').min(0).max(2).step(0.001)
 gui.add(unrealBloomPass, 'radius').min(0).max(2).step(0.001)
@@ -237,7 +239,46 @@ gui.add(tintPass.material.uniforms.uTint.value, 'x').min(-1).max(1).step(0.001).
 gui.add(tintPass.material.uniforms.uTint.value, 'y').min(-1).max(1).step(0.001).name('green')
 gui.add(tintPass.material.uniforms.uTint.value, 'z').min(-1).max(1).step(0.001).name('blue')
 
+//Displacement pass
 
+
+const DisplacementShader = {
+    uniforms: {
+        tDiffuse: { value: null },
+        uTime: { value: null }
+    },
+    vertexShader: `
+        varying vec2 vUv;
+
+        void main()
+        {
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+            vUv = uv;
+        }
+    `,
+    fragmentShader: `
+        uniform sampler2D tDiffuse;
+        uniform float uTime;
+
+        varying vec2 vUv;
+
+        void main()
+            {
+                vec2 newUv = vec2(
+                    vUv.x,
+                    vUv.y +sin(vUv.x * 10.0 + uTime) * 0.1
+                );
+                vec4 color = texture2D(tDiffuse, newUv);
+
+                gl_FragColor = color;
+            }
+    `
+}
+
+const displacementPass = new ShaderPass(DisplacementShader)
+displacementPass.material.uniforms.uTime.value = 0
+effectComposer.addPass(displacementPass)
 
 // SMAA pass
 if(renderer.getPixelRatio() === 1 && !renderer.capabilities.isWebGL2){
@@ -254,6 +295,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update passes
+    displacementPass.material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
